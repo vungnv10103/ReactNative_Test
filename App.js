88ca1aaa3413a15f77c3a00f22e7d628c581ed34
Vung Nguyen <vungvung1001@gmail.com>
2023-03-02 17:16:13 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function App () {
+    return (
+        <View style={{padding: 30}}>
+            <Text>Bài thi hết môn MOB306!</Text>
+        </View>
+    )
+};
